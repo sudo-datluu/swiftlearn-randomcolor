e715bee1ba5129fcb58dc3e6e2113b8709b6ab69
datluu <tad.louis2810@gmail.com>
2024-02-11 16:08:16 +1100

    init project, learn navigations

---
+++ RandomColor/ColorsDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorsDetailVC.swift
+//  RandomColor
+//
+//  Created by David on 11/2/2024.
+//
+
+import UIKit
+
+class ColorsDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RandomColor/ColorsTableVC.swift
@@ -0,0 +1,32 @@
+//
+//  ColorsTableVC.swift
+//  RandomColor
+//
+//  Created by David on 11/2/2024.
+//
+
+import UIKit
+
+class ColorsTableVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func tempButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
